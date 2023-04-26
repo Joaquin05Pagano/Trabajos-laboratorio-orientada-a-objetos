6587a61f54975ee7ad91e5c46a54f066e7592633
Joaquin05Pagano <131872083+Joaquin05Pagano@users.noreply.github.com>
2023-04-26 15:06:41 -0300

    Add files via upload

--- Conjetura de Collatz.cpp
+++ Conjetura de Collatz.cpp
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int collaz;
 int main(){
-    printf("Ingrese un numero: ");
+    printf("Ingrese un numero: ");          // se ingresa un numero y se escanea
     scanf("%d", &collaz);
 	do{
-        if(collaz %2 == 0){
-            collaz = collaz / 2;}
-        else if(collaz %2 != 0){
+        if(collaz %2 == 0){                 // se lee si el resto es 0, entonces es par y se lo divide por dos
+            collaz = collaz / 2;}           
+        else if(collaz %2 != 0){            //sino, es impar y se lo multiplica por 3 sumandole 1 al resultado
             collaz = (collaz * 3) + 1; }
-    printf("%d \n", collaz);
+    printf("%d \n", collaz);                //se van ingresando los resultados hasta llegar a 1, resultado final
     }while(collaz != 1);
 }
--- Gestión luminarias.cpp
+++ Gestión luminarias.cpp
@@ -1,45 +1,46 @@
 #include<stdio.h> 
-int maxcalle[5], luz, i, j, acum = 0, calle, totalluces, No_Encendidas;
+int maxcalle[5], luz, i, j, acum = 0, calle, totalluces, No_Encendidas,acumwhile = 0;
 
 int main() {
     for(i = 0; i < 5; i++){
         printf("\nIngrese la cantidad de luces que quiera en la calle N: %d: ", i);
-        scanf("%d", &maxcalle[i]);
-        acum = acum + maxcalle[i];
+        scanf("%d", &maxcalle[i]);                                                                          //se ingresa la canitdad de luces que quiere prendidas por calle
+        acum = acum + maxcalle[i];																            //acumulando todas las luces ingresadas para su maxima capacidad
         while(maxcalle[i] < 1 || maxcalle[i]> 15){
         	printf("\n---------------\nERROR");
-            printf("\n\nIngrese la cantidad de luces que quiera en la calle N: %d: ", i);
+            printf("\n\nIngrese la cantidad de luces que quiera en la calle N: %d: ", i);		            //esto es por si se ingresa un numero mayor a 15 o menor a 1, que le ingrese de nuevo
             scanf("%d", &maxcalle[i]);
+            acumwhile = acumwhile + maxcalle[i];
         }
     }
-    totalluces= 5 * 15;
-    No_Encendidas = totalluces - acum;
-    printf("\nIngrese la calle que quiera verificar (del 1 al 5): ");
-    scanf("%d", &calle);
+    acum = acum - acumwhile;
+    totalluces = 5 * 15;																		            //total de luces
+    No_Encendidas = totalluces - acum;      													            //total de luces apagadas
+    printf("\nIngrese la calle que quiera verificar (del 1 al 5): ");							            //se verifica la calle ingresada
+    scanf("%d", &calle);            
     while(calle < 1 || calle > 5){
         printf("\n---------------\nERROR");
-        printf("\nIngrese la calle que quiera verificar (del 1 al 5): ");
+        printf("\nIngrese la calle que quiera verificar (del 1 al 5): ");                                   //esto es por si se ingresa un numero mayor a 5 o menor a 1, que le ingrese de nuevo
         scanf("%d", &calle);
         }
-        printf("\n\nla calle numero %d tiene un numero de %d luces\n\n",calle - 1,maxcalle[calle - 1]);
-        printf("\nIngrese el numero de luz que quiera encender (1 al %d): ", maxcalle[calle - 1]);
+        printf("\n\nla calle numero %d tiene un numero de %d luces\n\n",calle - 1,maxcalle[calle - 1]);     //ingresa la calle seleccionada
+        printf("\nIngrese el numero de luz que quiera encender (1 al %d): ", maxcalle[calle - 1]);          //se pide la cantidad de luces que quiera encender en base al maximo indicado anteriormente
         scanf("%d", &luz);
         
         if( maxcalle[calle - 1] == luz || maxcalle[calle - 1] == 0){
-        printf("\n\nSe van a encender las luces maximas de esta cuadra");
+        printf("\n\nSe van a encender las luces maximas de esta cuadra");                                   //indica todas las luces que se van a encecender
         }
     while(luz > maxcalle[i - 1]){
         printf("\n---------------\nERROR");
-        printf("\nIngrese el numero de luz ya ingresado que quiera encender (1 al 15): ");
+        printf("\nIngrese el numero de luz que quiera encender (1 al %d): ", maxcalle[calle - 1]);          //esto es por si se ingresa un numero mayor a 15 o menor a 1, que le ingrese de nuevo
         scanf("%d", &luz);
     }
     printf("\n\n\nla cantidad de lucen encendidas es de: %d ", acum);
-    printf("\n\nla cantidad de luces apagadas es de: %d ", No_Encendidas);
-	printf("\n---------------------------\n");
+    printf("\n\nla cantidad de luces apagadas es de: %d ", No_Encendidas);                                  //ingresa cantidad de luces encendidas y apagadas
+	printf("\n---------------------------\n");  
     
-	printf("\n----problema----\n");
-	
-        if(luz == 0){
+	printf("\n\n----iluminado/no iluminado----\n\n");                                                       //aca va a indicar si en la calle esta bien distribuida la luz o no
+        if(luz == 0){                                                                                       //ingresando iluminado y en su caso contrario, no iluminado
             printf("\nEsta calle es oscura\n");
         }
         else if(luz == maxcalle[i - 1]){
